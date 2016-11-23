d1db78dbdabe131e1d8eda7196fe1bf0f8cd9816
Nikolett Pankovits <niki@technimo.net>
2016-11-23 17:19:41 -0500

    Fixed ActivityVC

--- iPractice/ActivityViewController.swift
+++ iPractice/ActivityViewController.swift
@@ -11,15 +11,57 @@ import Charts
 
 class ActivityViewController: UIViewController {
     
-    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var barChartView: BarChartView! {
+        didSet {
+            updateChart()
+        }
+    }
+    var months: [String]!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        barChartView.noDataText = "You haven't completed any tasks yet."
+    var dataSet: BarChartDataSet!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateChart()
+    }
 
+    func updateChart() {
+        
+        var values = [Int]()
+        
+//        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        
+//        let chartData = BarChartData(xVals: months, dataSet: dataSet)
+//        barChartView.data = chartData
+        
+        // initialize month data to 12 zeros
+        for _ in 0..<12 {
+            values.append(0)
+        }
+        
+        // read completion dates of tasks and update the values at the correct month
+        for task in AllTasks.shared.list {
+            for complDate in task.completionDates {
+                let month = Int(complDate.month) - 1
+                values[month] = values[month] + 1
+            }
+        }
+
+        // convert data for the Chart
+        var entries: [ChartDataEntry] = Array()
+        
+        for (i, value) in values.enumerated() {
+            entries.append(BarChartDataEntry(x: Double(i), y: Double(value)))
+        }
+        
+        dataSet = BarChartDataSet(values: entries, label: "One completed task")
+        
+        let data = BarChartData(dataSet: dataSet)
+        barChartView.backgroundColor = UIColor.red
+        barChartView.leftAxis.axisMinimum = 0.0
+        barChartView.rightAxis.axisMinimum = 0.0
+        barChartView.data = data
     }
     
 }
-
-
 
