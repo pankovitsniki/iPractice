83d78e0f5bf12676286050337564808b7b560bc5
Nikolett Pankovits <niki@technimo.net>
2016-11-30 18:42:01 -0500

    Added fake data

--- iPractice/AllTasks.swift
+++ iPractice/AllTasks.swift
@@ -57,6 +57,32 @@ class AllTasks: NSObject {
         if filemgr.fileExists(atPath: dataFilePath) {
             let dataArray = NSKeyedUnarchiver.unarchiveObject(withFile: dataFilePath) as! [Task]
             list.append(contentsOf: dataArray)
+        } else {
+            // add fake data
+            let complDateTest1 = "09-10-2016"
+            let complDateTest2 = "02-10-2016"
+            let complDateTest3 = "02-10-2016"
+            let complDateTest4 = "04-10-2016"
+            let complDateTest5 = "04-10-2016"
+            let complDateTest6 = "05-10-2016"
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM-dd-yyyy"
+            let date1 = dateFormatter.date(from: complDateTest1)
+            let date2 = dateFormatter.date(from: complDateTest2)
+            let date3 = dateFormatter.date(from: complDateTest3)
+            let date4 = dateFormatter.date(from: complDateTest4)
+            let date5 = dateFormatter.date(from: complDateTest5)
+            let date6 = dateFormatter.date(from: complDateTest6)
+            let taskTest1 = Task.init(name: "Sample Test 1")
+            let taskTest2 = Task.init(name: "Sample Test 2")
+            taskTest1.completionDates.append(date1!)
+            taskTest1.completionDates.append(date2!)
+            taskTest1.completionDates.append(date3!)
+            taskTest2.completionDates.append(date4!)
+            taskTest2.completionDates.append(date5!)
+            taskTest2.completionDates.append(date6!)
+            list.append(taskTest1)
+            list.append(taskTest2)
         }
     }
     
